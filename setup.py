a655630cb39cc0ea10aa65dd5bbfc6db4aec9e8c
clement <clement.bataille@epita.fr>
2025-11-18 00:07:39 +0100

    dummer

--- setup.py
+++ setup.py
@@ -1,9 +1,8 @@
-
 from setuptools import setup, find_packages
 
 setup(
-    name='my_package',
-    version='0.1.0',
+    name='terminapy',
+    version='0.0.1',
     packages=find_packages(),
     install_requires=[
     ]

8d8b119a6a1c9e53d39997d9b9592cb5512d2883
Klem10013 <77984717+Klem10013@users.noreply.github.com>
2025-12-02 00:10:00 +0100

    Bump version from 0.0.2 to 0.0.2.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r', encoding='utf-8') as f:
 
 setup(
     name='terminapy',
-    version='0.0.2',
+    version='0.0.2.1',
     packages=find_packages(),
     install_requires=[
     ],
